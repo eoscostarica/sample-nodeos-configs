4e22668abfd8adda10fd0066480d67fb85c89c2c
Xavier Fernandez <xavier@crservers.com>
2021-12-17 10:22:13 -0600

    feat(scripts): add example claimer script

---
+++ scripts/claimrewards.sh
@@ -0,0 +1,8 @@
+
+#/bin/bash
+date
+#Unlock wallet
+cleos wallet unlock -n claim --password $WALLET_PWD #Uses local ENV variable
+#Claim
+cleos push action eosio claimrewards '{"owner":"blockproduce"}' -p blockproduce@claimer # Add your own account name here``
+cleos wallet lock -n claim
